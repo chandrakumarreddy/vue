e1229d703ed596d8e2fd8c47c54a85b5a413fb30
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-08-17 23:15:51 +0530

    task-5 added

---
+++ Third_react_project/task-05/bundle.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: "#app",
+	data: {
+		text: "chandra",
+		finishedLink: "<a href='http://nitrr.ac.in' target='_blank'>Nitrr</a>"
+	},
+	methods: {
+		sayHello: function() {
+			alert("Hello");
+		}
+	}
+});
